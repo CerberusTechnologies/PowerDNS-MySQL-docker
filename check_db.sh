84afb50516303bb4726f5c5318cacae3eee6c0ed
Derek Vance <dvance@cerb-tech.com>
2017-05-03 16:30:38 -0400

    Fiddling with some idears

--- check_db.sh
+++ check_db.sh
@@ -7,7 +7,7 @@ PASSWORD=$4
 DATABASE=$5
 
 RESULT="mysql -h $HOST -P $PORT -u $USER -p$PASSWORD --skip-column-names -e \"SHOW DATABASES LIKE '$DATABASE'\""
-if [ "$RESULT" -ne "$DATABASE" ]; then
+if [ "$RESULT" != "$DATABASE" ]; then
     mysql -h "$HOST" -P "$PORT" -u "$USER" -p"$PASSWORD" -e "CREATE DATABASE '$DATABASE'" &&
 	mysql -h "$HOST" -P "$PORT" -u "$USER" -p"$PASSWORD" "$DATABASE" < /tmp/schema.sql;
 fi
